c36b2f5eaf0bbcfec6fc3003964a15c4e052ae9e
doehy <dbehxks02@naver.com>
2022-02-04 19:53:56 +0900

    Add files upload

--- BaekJoon/1966.py
+++ BaekJoon/1966.py
@@ -3,20 +3,20 @@ Number = int(input())
 for i in range(Number):
     N,M = map(int,input().split())
     printList = list(map(int,input().split()))
-    count = 0 #몇 번째로 출력되는지 알려줌
+    num = 0 #몇 번째로 출력되는지 알려줌
     checkList = [0 for _ in range(N)] 
     checkList[M] = 1 # 궁금한 문서위치 저장
 
     count=0
     while True:
         if printList[0] == max(printList):
-            count+=1
+            num+=1
 
             if checkList[0] != 1:
                 del printList[0]
                 del checkList[0]
             else:
-                print(count)
+                print(num)
                 break
         else:
             printList.append(printList[0])
