f5ffdb96dcd80f7fd1069a00dfffea423a336235
doehy <dbehxks02@naver.com>
2022-02-04 19:52:40 +0900

    Add files upload

---
+++ BaekJoon/1966.py
@@ -0,0 +1,25 @@
+Number = int(input())
+
+for i in range(Number):
+    N,M = map(int,input().split())
+    printList = list(map(int,input().split()))
+    count = 0 #몇 번째로 출력되는지 알려줌
+    checkList = [0 for _ in range(N)] 
+    checkList[M] = 1 # 궁금한 문서위치 저장
+
+    count=0
+    while True:
+        if printList[0] == max(printList):
+            count+=1
+
+            if checkList[0] != 1:
+                del printList[0]
+                del checkList[0]
+            else:
+                print(count)
+                break
+        else:
+            printList.append(printList[0])
+            checkList.append(checkList[0])
+            del printList[0]
+            del checkList[0]
--- code up/pratice.py
+++ code up/pratice.py
@@ -1,19 +1,6 @@
-def isSurprise(n):
-    for i in range(0,len(n)-1):
-        tmp = list()
-        for j in range(len(n)-(i+1)):
-            s = n[j] + n[j+i+1]
-            if s in tmp:
-                return False
-            tmp.append(s)
+N = 4
 
-    return True
+checkList = [1 for _ in range(N)]
 
-while True:
-    n = input()
-    if n == '*':
-        break
-    if isSurprise(n)==True:  
-        print(f"{n} is surprising.") 
-    if isSurprise(n)==False:
-        print(f"{n} is NOT surprising.")
+print(checkList.pop())
+print(checkList)  
