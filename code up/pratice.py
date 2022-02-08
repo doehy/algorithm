ae591fa1fafce71d308e8b6aef9d77a5b0d8809f
doehy <dbehxks02@naver.com>
2022-02-08 18:26:34 +0900

    first commit

--- code up/pratice.py
+++ code up/pratice.py
@@ -1,10 +1,4 @@
-number = int(input())
+queue = list()
 
-lst = list(map(int,input().split()))
-
-n = len(lst)
-
-if n == 1:
-    print(lst[0]**2)
-if n>=2:
-    print(max(lst)*min(lst))
+if queue:
+    print(1)
