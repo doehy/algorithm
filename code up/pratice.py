c278e03d5e14e6a145caf072e97124528868d461
doehy <dbehxks02@naver.com>
2022-02-10 18:19:02 +0900

    first commit

--- code up/pratice.py
+++ code up/pratice.py
@@ -1,3 +1,19 @@
-from theater_module import price,price_morning
-price(3)
-price_morning(4)
+N = int(input())
+
+lst = list()
+
+number = 0
+thtn = 0
+
+lst = list(map(int,input().split()))
+for i in range(len(lst)):
+  number = 0
+  if lst[i] == 1:
+    continue
+  for j in range(lst[i]):
+    if lst[i] % (j+1) == 0:
+      number+=1
+  if number == 2:
+      thtn+=1      
+
+print(thtn)
