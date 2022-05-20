cb0daac6687fb5ed81f322f65c929b76e5df85cb
doehy <dbehxks02@naver.com>
2022-05-20 11:45:20 +0900

    Add files via upload

--- code up/op.py
+++ code up/op.py
@@ -1,7 +1,22 @@
-number = [1,2,3,4,5]
+tong = [7,5,6,4,2,3,7,5]
 
-print(len(number))
+num = []
 
-b_number = [5,4,3,2,1]
 
-print(len(number))
+
+for i in tong:
+    flag = 0
+    if len(num) == 0:
+        num.append(i)
+    else:
+        for j in range(len(num)):
+            if num[j] + i <= 10:
+                print("이건 더해지는 것",i)
+                num[j] += i
+                flag = 1
+                break
+        if flag == 0:
+            print("이건 추가되는 것",i)
+            num.append(i)
+
+print(num)
