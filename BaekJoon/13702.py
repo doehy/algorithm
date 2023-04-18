53b5bd5aff607a8d55f68b466287b5db1215facf
doehy <87864025+doehy@users.noreply.github.com>
2023-04-18 16:16:53 +0900

    Add files via upload

---
+++ BaekJoon/13702.py
@@ -0,0 +1,23 @@
+n,k = map(int,input().split())
+
+data = []
+
+for i in range(n):
+    data.append(int(input()))
+
+data.sort()
+
+left, right = 1, data[-1]
+answer = 0
+while left <= right:
+    mid = (left + right) // 2
+    count = 0
+    for num in data:
+        count += num // mid
+    if count >= k:
+        answer = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+
+print(answer)
