7793f01617792462597c677e959af90ae8dbb512
doehy <87864025+doehy@users.noreply.github.com>
2023-06-15 12:31:05 +0900

    Add files via upload

---
+++ BaekJoon/1263.py
@@ -0,0 +1,18 @@
+import sys
+n = int(sys.stdin.readline())
+time = []
+
+for _ in range(n):
+  time.append(list(map(int,sys.stdin.readline().split())))
+
+time.sort(key = lambda x: x[1], reverse = True)
+
+ans = time[0][1] - time[0][0]
+
+for i in range(1,n):
+  if ans > time[i][1]:
+    ans = time[i][1] - time[i][0]
+  else:
+    ans -= time[i][0]
+
+print(ans) if ans >= 0 else print(-1)
