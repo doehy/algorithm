af70c03b6b57fba23bd1331f7ae0ba27a09215fe
doehy <87864025+doehy@users.noreply.github.com>
2023-07-21 20:14:55 +0900

    Add files via upload

---
+++ Programmers/recordRanking.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from itertools import combinations
+from bisect import bisect_left
+def solution(information, query):
+    answer = []
+    dic = defaultdict(list)
+    for info in information:
+        info = info.split()
+        text = info[:-1]
+        score = int(info[-1])
+        for i in range(1,5):
+            for lis in combinations(text,i):
+                dic[''.join(lis)].append(score)
+        dic[""].append(score)
+    for value in dic.values():
+        value.sort()
+        
+    for orders in query:
+        order = orders.replace("and ", "")
+        order = order.replace("-", "")
+        order = order.split()
+        if len(order) == 1:
+            target = dic[""]
+            idx = bisect_left(target, int(order[0]))
+            answer.append(len(target) - idx)
+            continue
+        key = ''.join(order[:-1])
+        targetScore = int(order[-1])
+        count = 0
+        if key in dic:
+            target = dic[key]
+            idx = bisect_left(target, targetScore)
+            count = len(target) - idx
+        answer.append(count)
+    return answer
