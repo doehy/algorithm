6646a8227f0af66b630722cab20342aaef797453
doehy <87864025+doehy@users.noreply.github.com>
2023-10-24 23:31:25 +0900

    Add files via upload

---
+++ BaekJoon/9465.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    data = [list(map(int,input().split())) for _ in range(2)]
+    dp = [[0] * n for _ in range(2)]
+    if n < 2:
+        print(max(data[0][0],data[1][0]))
+    else:
+        dp[0][0],dp[1][0] = data[0][0],data[1][0]
+        dp[0][1],dp[1][1] = data[0][1]+data[1][0],data[1][1]+data[0][0]
+        for i in range(2,n):
+            dp[0][i] = max(data[0][i]+dp[1][i-2],data[0][i]+dp[1][i-1])
+            dp[1][i] = max(data[1][i]+dp[0][i-2],data[1][i]+dp[0][i-1])
+        print(max(dp[0][n-1],dp[1][n-1]))
