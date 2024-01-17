18884d7b16fc577e7ea83eb6140057ee172cef3c
doehy <87864025+doehy@users.noreply.github.com>
2024-01-17 23:25:20 +0900

    Add files via upload

--- BaekJoon/14722.py
+++ BaekJoon/14722.py
@@ -1,24 +1,26 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-mat = [list(map(int, input().split())) for _ in range(N)]
-dp = [[[0, -1] for _ in range(N+1)] for _ in range(N+1)]
+n = int(input())
+data = [list(map(int, input().split())) for _ in range(n)]
+dp = [[[0, -1] for _ in range(n+1)] for _ in range(n+1)]
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        left, up = dp[i][j-1], dp[i-1][j]
-        left_check = (left[1]+1)%3 == mat[i-1][j-1]
-        up_check = (up[1]+1)%3 == mat[i-1][j-1]
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        leftCheck = (dp[i][j-1][1]+1)%3 == data[i-1][j-1]
+        upCheck = (dp[i-1][j][1]+1)%3 == data[i-1][j-1]
 
-        left_score = left[0] + left_check
-        up_score = up[0] + up_check
-        if left_score > up_score:
-            dp[i][j][0] = left_score
-            dp[i][j][1] = mat[i-1][j-1] if left_check else left[1]
+        leftMilk = dp[i][j-1][0] + leftCheck
+        upMilk = dp[i-1][j][0] + upCheck
 
+        if leftMilk > upMilk:
+            dp[i][j][0] = leftMilk
+            dp[i][j][1] = data[i-1][j-1] if leftCheck else dp[i][j-1][1]
         else:
-            dp[i][j][0] = up_score
-            dp[i][j][1] = mat[i-1][j-1] if up_check else up[1]
+            dp[i][j][0] = upMilk
+            dp[i][j][1] = data[i-1][j-1] if upCheck else dp[i-1][j][1]
 
-print(dp[N][N][0])
+print(dp[n][n][0])
+
+
+# 그냥 결국 왼쪽, 위와 비교해서 더 큰것을 가져온다
